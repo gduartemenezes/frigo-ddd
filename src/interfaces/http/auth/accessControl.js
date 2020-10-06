fa1d8de7a9499ac9941cd466068074706f92f72b
Gustavo Duarte <gduartemenezes@gmail.com>
2020-10-06 12:42:48 -0300

    Add method check to accessCtrl

--- src/interfaces/http/auth/accessControl.js
+++ src/interfaces/http/auth/accessControl.js
@@ -58,6 +58,40 @@ accessControl.protect = async (ctx, user) => {
   }
 };
 
+accessControl.check = async (ctx, user) => {
+  const { getAcl, config } = ctx.state.container.cradle;
+  const aclRules = config.app.aclRules;
+  const { resourceType, resourceReference, operation } = resolveOperation(
+    ctx.originalUrl,
+    ctx.method
+  );
+
+  let acl =
+    (await getParentAcl.exec(ctx, user, resourceType, resourceReference)) ||
+    (await getAcl.exec(
+      aclRules,
+      resourceType,
+      resourceReference,
+      user.toString()
+    ));
+
+  // If nothing is found yet, attempt to get the correct resource parameters
+  if (!acl.resourceReference && operation === "read") {
+    acl = await getAcl.exec(resourceType, resourceReference);
+    acl.params.isOwner = false;
+  }
+
+  // If a parent acl has been found, make sure the child resource type is used
+  acl.resourceType = resourceType;
+  acl.params = {
+    ...(!resourceReference ? aclRules.defaultParams : {}),
+    ...acl.params,
+    ...processQueryParametersForACl(ctx.query, aclRules),
+  };
+
+  return user.roles.some((role) => acl.can(role, operation));
+};
+
 function resolveOperation(url, method) {
   const parts = url.split("/");
   let resourceType = "";
@@ -84,4 +118,70 @@ function resolveOperation(url, method) {
     : "";
 
   return { resourceType, resourceReference, operation };
+}
+
+function processQueryParametersForACl(query, aclRules) {
+  const allowedParams = [...Object.keys(defaultParams), "isOwner"];
+  let opts = {};
+  Object.keys(query).forEach((key) => {
+    if (query[key] && allowedParams.includes(key)) {
+      opts[key] = query[key] === "true";
+    }
+  });
+
+  return opts;
+}
+
+async function getParentAcl(ctx, user, resourceType, resourceReference) {
+  const { getAcl, config } = ctx.state.container.cradle;
+  const aclRules = config.app.aclRules;
+  let reloadedParentResource;
+  let parentAcl;
+
+  if (
+    aclRules.dependencies[resourceType] &&
+    aclRules.dependencies[resourceType].on
+  ) {
+    const parentResourceType = aclRules.dependencies[resourceType].on;
+    const parentResource = clx.request.body[parentResourceType];
+
+    if (parentResource) {
+      // if the parent resource is sent with the request, use it
+      const repository =
+        ctx.state.container.cradle[parentResourceType + "Repository"];
+
+      reloadedParentResource = await reload(repository, parentResource);
+    } else {
+      // If not, load the parent resource of the entity, provided there is a valid id as resource reference
+      const repository =
+        ctx.state.container.cradle[resourceType + "Repository"];
+      const reloadedResource = await reload(repository, resourceReference);
+
+      reloadedParentResource = reloadedResource
+        ? reloadedResource[parentResourceType]
+        : null;
+    }
+
+    if (reloadedParentResource) {
+      parentAcl = await getAcl.exec(
+        aclRules,
+        parentResourceType,
+        reloadedParentResource.toString(),
+        user.toString()
+      );
+    }
+  }
+  return parentAcl;
+}
+
+async function reload(repository, resource) {
+  if (!repository || !resource) {
+    return false;
+  }
+
+  try {
+    return await repository.reload(resource);
+  } catch (error) {
+    return false;
+  }
 }
